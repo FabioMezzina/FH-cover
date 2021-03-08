27d4c8bf31870e2f1e93ead31b2cf48893197ee5
FabioMezzina <fabio.mezzina.09@gmail.com>
2021-03-08 16:26:28 +0100

    FH-cover

---
+++ src/App.vue
@@ -0,0 +1,120 @@
+<template>
+  <div id="app">
+    <div class="container">
+      <div class="user-input">
+        <div class="input-wrapper">
+          <label for="first-name">First Name*</label>
+          <input id="first-name" type="text" v-model="firstName" placeholder="First name here">
+        </div>
+        <div class="input-wrapper">
+          <label for="last-name">Last Name*</label>
+          <input id="last-name" type="text" v-model="lastName" placeholder="Last name here">
+        </div>
+        <div class="input-wrapper">
+          <label for="prefer-framework">JS Framework*</label>
+          <input id="prefer-framework" type="text" v-model="jsPreferFw" placeholder="JS favourite framework">
+        </div>
+      </div>
+      <Cover
+        :firstName="firstName"
+        :lastName="lastName"
+        :jsPreferFw="jsPreferFw"
+      />
+    </div>
+    <div class="change-me">
+      *Feel free to change input data
+    </div>
+  </div>
+</template>
+
+<script>
+import Cover from './components/Cover.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Cover
+  },
+  data() {
+    return {
+      firstName: 'Fabio',
+      lastName: 'Mezzina',
+      jsPreferFw: 'Vue.js'
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+#app {
+  position: relative;
+  min-height: 100vh;
+  min-height: 100vh;
+  font-family: 'Courier New', Courier, monospace;
+  color: #fff;
+  background-image: linear-gradient(180deg,rgba(83,47,248,.82),#4c1ff2);
+  .container {
+    display: flex;
+    flex-direction: column;
+    margin: 0 auto;
+    padding: 0 2em;
+    max-width: 1200px;
+    min-height: 100%;
+    // user input
+    .user-input {
+      display: flex;
+      flex-wrap: wrap;
+      justify-content: space-evenly;
+      min-height: 25vh;
+      margin: 1em 0;
+      .input-wrapper {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+        flex-basis: calc(100% / 3);
+        margin: auto 0;
+        padding: 1em;
+        label {
+          font-size: 1.1rem;
+          font-weight: 600;
+          margin-bottom: .5em;
+          letter-spacing: .1em;
+          color: rgb(255, 186, 0);
+        }
+        input {
+          padding: .8em;
+          font-size: 1.2rem;
+          text-align: center;
+          color: #ff7575;
+          background: transparent;
+          border: 2px solid rgba(255, 255, 255, .4);
+          border-radius: 1.6em;
+          outline: none;
+          &:focus {
+            background-color: rgba(255, 255, 255, .05);
+          }
+        }
+      }
+    }
+  }
+
+  // welcome modal
+  .change-me {
+    display: block;
+    position: absolute;
+    top: 0;
+    right: 0;
+    padding: .3em .5em;
+    color: rgb(255, 186, 0);
+    background-color: rgba(0, 0, 0, .4);
+    border-bottom-left-radius: .7em;
+    z-index: 1;
+  }
+}
+</style>
---
+++ src/components/Cover.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="cover">
+    <p>Buongiorno,</p>
+    <p>Sono <span>{{ firstName }}</span>, junior Full Stack Web developer che è rimasto particolarmente colpito dalle attività che effettua la vostra azienda. Ho molto apprezzato l'apertura all'innovazione nel portare nei vostri progetti tecnologie sempre più innovative, in particolare la scelta dell’utilizzo di <span>{{ jsPreferFw }}</span> come framework JavaScript.
+    </p>
+
+    <p>Da sempre appassionato di programmazione, ho deciso di trasformare questa passione in lavoro.
+        Ho fatto formazione nello sviluppo web inizialmente da autodidatta, successivamente frequentando il corso da Full Stack Web Developer di Boolean.careers (corso full time della durata di sei mesi) grazie al quale ho consolidato ed esteso grandemente le mie conoscenze e capacità (<a href="https://www.boolean.careers">www.boolean.careers</a>).</p>
+
+    <p>Curioso di natura. Mi piace provare, sbagliare e affrontare difficoltà, è una forte spinta ad esplorare ciò che mi è ignoto per apprenderne sempre il più possibile.</p>
+
+    <p>Certo che una nostra collaborazione possa essere un'ottima opportunità vi invio il mio curriculum vitae da sottoporre alla vostra attenzione. </p>
+    <p>Resto in attesa di un vostro cortese riscontro. <br>
+      Cordialmente, <br>
+      <span>{{ firstName }} {{ lastName }}</span>
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'cover',
+  props: ['firstName', 'lastName', 'jsPreferFw']
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+.cover {
+  margin: auto;
+  p {
+    font-size: 1.3rem;
+    line-height: 1.5em;
+    margin-bottom: 1rem;
+  }
+  a {
+    text-decoration: none;
+    color: #fff;
+    &:hover {
+      text-decoration: underline;
+    }
+  }
+  span {
+    color: #000;
+    font-size: 1.1em;
+    &:hover {
+      color: #fff;
+    }
+  }
+}
+</style>
